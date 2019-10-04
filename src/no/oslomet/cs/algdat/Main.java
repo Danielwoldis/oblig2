36c534e75bc3d767bd07e8b77520656e03efa032
OleLøc <olebastiank.lochen@gmail.com>
2019-10-04 14:09:39 +0200

    begynt på 3 står fast

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -7,15 +7,16 @@ public class Main extends DobbeltLenketListe{
     public static void main(String[] args) {
         //System.out.println("noe");
 
-        Integer[] a = {null, 1, null};
+        Integer[] a = {1, 2, 3, 4};
         Objects.requireNonNull(a, "Tabellen har ingen verdier!");
 
         DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>(a);
         //System.out.println(liste.antall());
 
         Liste<String> liste1 = new DobbeltLenketListe<>();
-        System.out.println(liste.antall() + " " + liste.tom());
+        //System.out.println(liste.antall() + " " + liste.tom());
 
+        System.out.println(liste.hent(1));
     }
 }
 
