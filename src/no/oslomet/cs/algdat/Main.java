484d2fe2165d7805fcc659e50979f141756b32d7
danielwoldis <Danielwoldegiorgis@hotmail.com>
2019-10-08 14:05:47 +0200

    Merge branch 'master' of C:\Users\danie\Desktop\skole\algdat\oblig2 with conflicts.

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -15,8 +15,6 @@ public class Main extends DobbeltLenketListe{
 
         String[] navn = {"Lars","Anders","Bodil","Kari","Per","Berit"};
 
-        Integer[] tall = {1};
-
         //DobbeltLenketListe<Character> liste = new DobbeltLenketListe<>(characters);
         /*
         String[] s1 = {};
@@ -26,23 +24,17 @@ public class Main extends DobbeltLenketListe{
         DobbeltLenketListe<Integer> l2 = new DobbeltLenketListe<>(s2);
         DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
 
-        `
-        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>();
+        `*/
+        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>(new Integer[]{1,2,4,5,67,7});
+        Integer n=7;
+        Integer n1=67;
+        liste.fjern(n);
+        liste.fjern(n1);
+        System.out.println(liste.toString());
+        System.out.println(liste.omvendtString());
 
-        liste.leggInn(0, 4);  // ny verdi i tom liste
-        liste.leggInn(0, 2);  // ny verdi legges forrest
-        liste.leggInn(2, 6);  // ny verdi legges bakerst
-        liste.leggInn(1, 3);  // ny verdi nest forrest
-        liste.leggInn(3, 5);  // ny verdi nest bakerst
-        liste.leggInn(0, 1);  // ny verdi forrest
-//        liste.leggInn(6, 7);  // ny verdi legges bakerst
-*/
-        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>(tall);
-        System.out.println(liste.toString() + " " + liste.antall());
-        liste.fjern(new Integer(1));
-        System.out.println(liste.toString() + " " + liste.antall());
-        liste.leggInn(5);
-        System.out.println(liste.toString()+ " " + liste.antall());
+
+
 
 
 
